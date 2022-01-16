5c5b0073ac6da74f2fc58711647e25a9887d38f8
rumyantsev <LemToUp@users.noreply.github.com>
2022-01-16 21:51:58 +0300

    Apply typeorm to Columns section

--- src/resources/column/column.service.ts
+++ src/resources/column/column.service.ts
@@ -1,45 +1,78 @@
-import * as columnsRepo from './column.memory.repository';
-import {IColumnData} from "./column.model";
-/**
- * @description Get all Columns
- *
- * @return Promise list of the Columns
- */
-export const getAll = (): Promise<Array<IColumnData>> => columnsRepo.getAll();
-/**
- * @description Get Column by id
- *
- * @param id string
- *
- * @return Promise Column | false
- */
-export const get = (id: string): Promise<IColumnData|false> => columnsRepo.get(id);
-/**
- * @description Create Column
- *
- * @param title string
- * @param order string
- *
- * @return Promise created Column | false
- */
-export const create = (title: string, order: string): Promise<IColumnData> => columnsRepo.create(title, order);
-/**
- * @description Update Column
- * @param id string
- * @param title string|undefined
- * @param order string|undefined
- *
- * @return Promise updatedColumn | false
- */
-export const update = (id: string, title: string | null = null, order: string | null = null): Promise<IColumnData|false> => columnsRepo.update(id, {
-    ...(title ? { title } : {}),
-    ...(order ? { order } : {}),
-})
-/**
- * @description Delete Column
- *
- * @param id string
- *
- * @return Promise deleted column | false
- */
-export const remove = (id: string): Promise<IColumnData|false> => columnsRepo.remove(id);
+import { getCustomRepository } from 'typeorm';
+import { ColumnRepository } from './column.repository';
+import { ColumnModel } from './column.model';
+
+
+export class ColumnService {
+    columnRepository: ColumnRepository = getCustomRepository(ColumnRepository);
+
+    /**
+     * @description Get all Columns
+     *
+     * @return Promise list of the Columns
+     */
+    getAll() {
+        return this.columnRepository.getAll()
+    };
+
+    /**
+     * @description Get Column by id
+     *
+     * @param id string
+     *
+     * @return Promise Column | false
+     */
+    get(id: string) {
+        return this.columnRepository.get(id)
+    };
+
+    /**
+     * @description Create Column
+     *
+     * @param title string
+     * @param order number
+     *
+     * @return Promise created Column | false
+     */
+    create(title: string, order: number) {
+        const columnModel = new ColumnModel();
+
+        columnModel.title = title;
+        columnModel.order = order;
+
+        return this.columnRepository.save(columnModel)
+    };
+
+    /**
+     * @description Update Column
+     * @param id string
+     * @param title string|undefined
+     * @param order number|undefined
+     *
+     * @return Promise updatedColumn | false
+     */
+    async update(id: string, title?: string, order?: number) {
+        const columnModel = await this.get(id);
+
+        if (columnModel) {
+            return this.columnRepository.update(id, {
+                title: title || columnModel.title,
+                order: order || columnModel.order,
+            })
+        }
+
+        return false;
+
+    }
+
+    /**
+     * @description Delete Column
+     *
+     * @param id string
+     *
+     * @return Promise deleted column | false
+     */
+    remove(id: string) {
+        return this.columnRepository.delete(id);
+    };
+}
