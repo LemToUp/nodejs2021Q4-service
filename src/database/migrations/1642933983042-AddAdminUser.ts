c18a9bef693294fc8f35bdee04db079271619df5
rumyantsev <LemToUp@users.noreply.github.com>
2022-01-30 21:07:07 +0300

    Add Db module

--- src/database/migrations/1642933983042-AddAdminUser.ts
+++ src/database/migrations/1642933983042-AddAdminUser.ts
@@ -3,16 +3,15 @@ import {UserService} from "../../resources/users/user.service";
 require('dotenv').config();
 
 export class AddAdminUser1642933983042 implements MigrationInterface {
+    constructor(private userService: UserService) {}
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        const userService = new UserService();
-        await userService.create('admin', 'admin', 'admin');
+        await this.userService.create('admin', 'admin', 'admin');
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        const userService = new UserService();
-        const adminUser = await userService.getByLogin('admin');
-        adminUser && adminUser.id && await userService.remove(adminUser.id);
+        const adminUser = await this.userService.getByLogin('admin');
+        adminUser && adminUser.id && await this.userService.remove(adminUser.id);
     }
 
 }
